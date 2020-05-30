b04ad1de632f8b38a1f3777f495a91a1f9f82355
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2020-05-29 23:47:50 -0400

    Add Async and App POST Request

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
@@ -17,9 +17,10 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
+const async = require('async');
+
 // Initialize the main project folder
 app.use(express.static('website'));
-
 
 // Setup Server
 const port = 8000;
@@ -29,5 +30,18 @@ const server = app.listen(port, listening);
 function listening() {
   // console.log(server);
   console.log('server running');
-  console.log('running on localhost: ${port}');
+  console.log(`running on localhost: ${port}`);
 }
+
+//Create post() with url path and a callback function
+app.post('/addZipCode', addZipCode);
+
+// Create a post route for adding zipcodes to the projectData array
+function addZipCode (req, res) {
+  const body = req.body
+  projectData.zipCode = body.zipCode;
+  res.send('Test the POST route');
+  projectData.push(req.body);
+  console.log(projectData);
+  console.log('zipcode added');
+};
