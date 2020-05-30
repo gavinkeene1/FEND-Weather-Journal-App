47ebc9d898c467dcae132eb26d930c087cf9bae7
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2020-05-29 23:50:14 -0400

    Add POST and GET Requests For Project Data

--- website/app.js
+++ website/app.js
@@ -4,3 +4,34 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+  console.log('hello');
+
+const postProjectData = async (url = '', data) => {
+  console.log('hello');
+;
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // Body data type must match the "Content-Type" header
+        body: JSON.stringify(data)
+      });
+      return response;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const getProjectData = async url => {
+    try {
+      const newData = await fetch(url);
+      console.log(newData);
+      return newData;
+    } catch(error) {
+      console.log("error", error);
+    }
+};
